5d4d6d99e6f13ebc621397353a3c12896ce9d571
Richard <proxyserver7798@gmail.com>
2022-03-23 23:27:36 +0200

    Fixed the home tabs

--- src/components/features/landingPage/UpcomingEvents.js
+++ src/components/features/landingPage/UpcomingEvents.js
@@ -14,21 +14,30 @@ const UpcomingEvents = () => {
             id: uuidv4(),
             smallDate: "January 2022",
             bigDate: "12-01-2022",
-            story: "The Club is a unique platform for decision makers and influential members of the society."
+            story: "The Club is a unique platform for decision makers and influential members of the society. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         },
         {
             id: uuidv4(),
             smallDate: "February 2022",
             bigDate: "12-01-2022",
-            story: "The Club is a unique platform for decision makers and influential members of the society."
+            story: "The Club is a unique platform for decision makers and influential members of the society. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         },
         {
             id: uuidv4(),
             smallDate: "March 2022",
             bigDate: "12-01-2022",
-            story: "The Club is a unique platform for decision makers and influential members of the society."
+            story: "The Club is a unique platform for decision makers and influential members of the society. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         },
     ]) 
+
+    const stringSplitter = (str) => {
+        let mStr = ""
+        for(let i = 0; i < 90; i++) {
+            mStr += str.charAt(i)
+        }
+        return mStr
+    }
+
     return (
         <>
         <div className="uerTitle">Upcoming Events</div>
@@ -36,7 +45,10 @@ const UpcomingEvents = () => {
             <div className="upEventsLeft">
                 {
                     eventsData.map((item, index) => (
-                        <div className="uEventBox" style={{
+                        <div 
+                            className="uEventBox" 
+                            onClick={() => setSelected(index + 1)}
+                            style={{
                             opacity: selected === index + 1 ? "1" : "0.4"
                         }}>
                             <div className="uEvent_box_vSlide" style={{
@@ -47,8 +59,8 @@ const UpcomingEvents = () => {
                                 <div className="ue_paragraph">
                                     {/* The Club is a unique platform for decision makers and influential members of the society. */}
                                     {
-                                        item.story
-                                    }
+                                        stringSplitter(item.story)
+                                    }...
                                 </div>
                             </div>
                         </div>
@@ -58,18 +70,21 @@ const UpcomingEvents = () => {
             <div className="upEventsRight">
                 <div className="uer_mid_box">
                     <div className="uer_imgs_box">
-                        <img src={ueBanner} className="uer_big_img" />
+                        {/* <img src={ueBanner} className="uer_big_img" /> */}
+                        <img src={process.env.PUBLIC_URL + `/resources_folder/events/event${selected}.png`} className="uer_big_img" />
                         <img src={bph1_icon} className="uer_small_img" />
                     </div>
-                    <div className="uer_texts">
-                        <div className="uer_title_texts">
-                            <p className="uer_title_title">Annual General Meeting</p>
-                            <p className="uer_title_date">12-01-2022</p>
-                        </div>
-                        <p className="uer_title_story">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                        </p>
-                    </div>
+                    {
+                        eventsData.map((item, index) => (
+                            <div className={ index + 1 === selected ?  "uer_texts uer_texts_current":"uer_texts"} style={{display: index + 1 === selected ? "block":"none"}}>
+                                <div className="uer_title_texts">
+                                    <p className="uer_title_title">{item.title}</p>
+                                    <p className="uer_title_date">{item.bigDate}</p>
+                                </div>
+                                <p className="uer_title_story">{item.story}</p>
+                            </div>
+                        ))
+                    }
                 </div>
                 <div className="uer_dots"></div>
             </div>
@@ -93,12 +108,10 @@ const UpcomingEvents = () => {
                                     </div>
                                     <div className="mobi_texts">
                                         <div className="mobi_title_texts">
-                                            <p className="mobi_title_title">Annual General Meeting</p>
-                                            <p className="mobi_title_date">12-01-2022</p>
+                                            <p className="mobi_title_title">{item.title}</p>
+                                            <p className="mobi_title_date">{item.bigDate}</p>
                                         </div>
-                                        <p className="mobi_title_story">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                        </p>
+                                        <p className="mobi_title_story">{item.story}</p> 
                                     </div>
                                 </div>
                             </div>
