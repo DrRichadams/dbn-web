0086cbf0b0a6f71c0b9d4e07baa3d1d6d315415f
Anesu <ndoroansue01@gmail.com>
2022-03-24 00:33:21 +0200

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -28,6 +28,12 @@ const Wrapper = styled.section`
         background-position:center center;
         width:100%;
         height:80vh;
+        @media(min-width:768px){
+            height:500px;
+        }
+        @media(min-width:1200px){
+            height:80vh;
+        }
     }
     
 `
